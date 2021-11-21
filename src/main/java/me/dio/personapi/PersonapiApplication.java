c96df23b59836b7e3995d827ef9710a5b2b2b734
endeu <wendellpguariento@gmail.com>
2021-11-21 20:41:03 -0300

    settings for deploy in heroku

--- src/main/java/me/dio/personapi/PersonapiApplication.java
+++ src/main/java/me/dio/personapi/PersonapiApplication.java
@@ -2,12 +2,18 @@ package me.dio.personapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PersonapiApplication {
+class DemoApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PersonapiApplication.class, args);
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication .class);
 	}
 
-}
+public static void main(String[] args) {
+		SpringApplication.run(DemoApplication .class, args);
+		}
+		}
